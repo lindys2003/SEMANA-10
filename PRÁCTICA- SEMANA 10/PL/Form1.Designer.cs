060bf3f00b30f3c0e7b887178d15261720c2e73e
vane2810 <olgavanessasf@gmail.com>
2022-10-03 15:41:02 -0600

    Agregar archivos de proyecto.

---
+++ PRÁCTICA- SEMANA 10/PL/Form1.Designer.cs
@@ -0,0 +1,61 @@
+﻿
+namespace PRÁCTICA__SEMANA_10
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_Test = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btn_Test
+            // 
+            this.btn_Test.Location = new System.Drawing.Point(73, 59);
+            this.btn_Test.Name = "btn_Test";
+            this.btn_Test.Size = new System.Drawing.Size(169, 108);
+            this.btn_Test.TabIndex = 0;
+            this.btn_Test.Text = "Test";
+            this.btn_Test.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(332, 237);
+            this.Controls.Add(this.btn_Test);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_Test;
+    }
+}
+
